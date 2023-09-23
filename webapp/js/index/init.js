0a79b57d25ce8305c3efbfd7caf531f76bf7f22e
Luna <38033777+Hyejeong97@users.noreply.github.com>
2023-09-23 14:58:08 +0900

    Add files via upload

--- webapp/js/index/init.js
+++ webapp/js/index/init.js
@@ -177,7 +177,14 @@ $(document).ready(function(){
 
 
 
-
+	document.addEventListener("DOMContentLoaded", function () {
+	    const menuToggle = document.getElementById("menuToggle");
+	    const menu = document.querySelector("ul.menu");
+	
+	    menuToggle.addEventListener("click", function () {
+	        menu.classList.toggle("active");
+	    });
+	});
 
 
 
