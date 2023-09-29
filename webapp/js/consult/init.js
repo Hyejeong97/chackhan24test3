3d464171772b7428d64cb85f1a63782f4678728a
Luna <38033777+Hyejeong97@users.noreply.github.com>
2023-09-29 22:12:15 +0900

    Add files via upload

--- webapp/js/consult/init.js
+++ webapp/js/consult/init.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
 			alert("전송할 문자 내용을 입력해 주세요.");
 			return false;
 		}else{
-			$(".sms a").attr("href", "sms:010-6568-2360&body=" + content);
+			$(".sms a").attr("href", "sms:1588-7126&body=" + content);
 		}
 		
 	});
@@ -74,6 +74,29 @@ $(document).ready(function(){
 	    });
 	});
 
+
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	    mapOption = { 
+	        center: new kakao.maps.LatLng(37.5355793, 126.868904), // 지도의 중심좌표
+	        level: 3 // 지도의 확대 레벨
+	    };
+	
+	var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+	
+	// 마커가 표시될 위치입니다 
+	
+	var markerPosition  = new kakao.maps.LatLng(37.5355793, 126.868904); 
+	
+	// 마커를 생성합니다
+	var marker = new kakao.maps.Marker({
+	    position: markerPosition
+	});
+	
+	// 마커가 지도 위에 표시되도록 설정합니다
+	marker.setMap(map);
+	
+	// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+	// marker.setMap(null);   
 
 
 
